a9ebb737bbc8d24eb1dfd89613a8aa04da53983d
Randy <randy.bickham@gmail.com>
2024-02-05 14:30:58 -0600

    new folders

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function MyButton() {
+    return
+    <button>I'm a button</button>
+}
+
+export default function MyApp(){
+    return (
+        <div>
+            <h1>Welcome to the button page </h1>
+            <MyButton />
+        </div>
+    )
+}
